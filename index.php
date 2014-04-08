74eacc4f13602960bd08ab9c23d1e48d0897d6a3
Patrick Otto <predrag81@t-online.de>
2014-04-08 15:13:11 +0200

    beautifier

--- index.php
+++ index.php
@@ -1,95 +1,95 @@
 <?php 
+// MYSQL Konfig
+	$tabelle = ""; 	// Name der Tabelle  
+	$dbname  = ""; 	// name der Datenbank
+	$dbuname = ""; 	// Datenbank Username
+	$dbpass  = ""; 	// Datenbank User Passwort
+	$dbhost  = ""; 	// Adresse der Datenbank
 
-	// MYSQL Konfig
-	  $tabelle = ""; 	// Name der Tabelle  
-	  $dbname  = ""; 	// name der Datenbank
-	  $dbuname = ""; 	// Datenbank Username
-	  $dbpass  = ""; 	// Datenbank User Passwort
-	  $dbhost  = ""; 	// Adresse der Datenbank
-	  
 	// Verbindung zum MySQL Host aufbauen
-	  $verbindung = mysql_connect($dbhost,$dbuname,$dbpass);
-	  if (!$verbindung) {
-			die('Verbindung zur Datenbank auf '.$dbhost.' schlug fehl: ' . mysql_error());
-		}
+	$verbindung = mysql_connect($dbhost,$dbuname,$dbpass);
+	if (!$verbindung) {
+		die('Verbindung zur Datenbank auf '.$dbhost.' schlug fehl: ' . mysql_error());
+	}
 	//MySQL Datenbank öffnen
-	  $opendb = mysql_select_db($dbname); 
-	  if (!$opendb) {
-			die('Die Datenbank '.$dbname.' konnte nicht geöffnet werden!');
-		}	  
-	  
-		// Hier werden die Klassennummern der Api durch Namen ersetzt
-		function id2class($id){ 
-			if($id == 1)$val = "Krieger";
-			if($id == 2)$val = "Paladin";
-			if($id == 3)$val = "Jäger";
-			if($id == 4)$val = "Schurke";
-			if($id == 5)$val = "Priester";
-			if($id == 6)$val = "Todesritter";
-			if($id == 7)$val = "Schamane";
-			if($id == 8)$val = "Magier";
-			if($id == 9)$val = "Hexenmeister";
-			if($id == 10)$val = "Mönch";
-			if($id == 11)$val = "Druide";
-			return $val;
-		}
-		//Hier werden die Ränge durch Namen ersetzt
-		function id2rank($id){
-			if($id == 0)$val = "Gildenmeister";
-			if($id == 1)$val = "Offizier";
-			if($id == 2)$val = "Offizier";
-			if($id == 3)$val = "Member";
-			if($id == 4)$val = "Trial";
-			return $val;
-		}
-			
-		// Hilfsfunktion um Sonderzeichen korrekt zu Interpretieren
-		function convutf8($var){
-		return mb_convert_encoding($var, "UTF-8", "ISO-8859-1");
-		}
+	$opendb = mysql_select_db($dbname); 
+	if (!$opendb) {
+		die('Die Datenbank '.$dbname.' konnte nicht geöffnet werden!');
+	}	  
+  
+	// Hier werden die Klassennummern der Api durch Namen ersetzt
+	function id2class($id){ 
+		if($id == 1)$val = "Krieger";
+		if($id == 2)$val = "Paladin";
+		if($id == 3)$val = "Jäger";
+		if($id == 4)$val = "Schurke";
+		if($id == 5)$val = "Priester";
+		if($id == 6)$val = "Todesritter";
+		if($id == 7)$val = "Schamane";
+		if($id == 8)$val = "Magier";
+		if($id == 9)$val = "Hexenmeister";
+		if($id == 10)$val = "Mönch";
+		if($id == 11)$val = "Druide";
+		return $val;
+	}
+	//Hier werden die Ränge durch Namen ersetzt
+	function id2rank($id){
+		if($id == 0)$val = "Gildenmeister";
+		if($id == 1)$val = "Offizier";
+		if($id == 2)$val = "Offizier";
+		if($id == 3)$val = "Member";
+		if($id == 4)$val = "Trial";
+		return $val;
+	}
+		
+	// Hilfsfunktion um Sonderzeichen korrekt zu Interpretieren
+	function convutf8($var){
+	return mb_convert_encoding($var, "UTF-8", "ISO-8859-1");
+	}
 
-		function ausgabe($where)
-		{
-			GLOBAL $tabelle;
-			// Ausgabe Konfig für die SQL Abfrage
-			$max = 300;  					// Max Anzahl anzeigen
-			$order = "order by klasse, rank "; 	// Nach Rang sortieren
-			$groupby = "  ";  				// Optional z.B. "group by rank", dann wird jeder Rank nur mit einem Namen belegt
-			$sql = "SELECT * FROM ".$tabelle." ".$where." ".$groupby." ".$order." LIMIT ".$max; 
-				#echo $sql;		
-				$val = '';
-				$resultX = mysql_query($sql);
-					  while ($row = mysql_fetch_object($resultX))
-						{
-						
-						if($row->klasse != $oldklasse)
-							$val .= '<tr><th colspan=2>'.id2class($row->klasse).'</th></tr>';
-						
-						$val .= '
-								<tr>								
-									<td valign=top>
-										<div class="char">
-										<div class="level">'.$row->level.'</div><div class="name"><b><a href ="http://eu.battle.net/wow/de/character/blackmoore/'.convutf8($row->name).'/advanced">'.convutf8($row->name).'</a>&nbsp;&nbsp;&nbsp;&nbsp;</b></div><div class="points"><img src="http://eu.battle.net/wow/static/images/icons/achievements.gif" alt=""> </img>'.$row->points.'</div>					
-										<div class="specimg"><img src="http://media.blizzard.com/wow/icons/36/'.$row->specimg.'.jpg"><img></div><div class="spec">'.$row->spec.'</div><div class="rank">'.id2rank($row->rank).'</div>
-										</div>
-										<div class="img-polaroid"><img src="http://eu.battle.net/static-render/eu/'.$row->tbnail.'"></img></div>
-									</td>
-								</tr>
-						';
-						
-						$oldklasse = $row->klasse;
-						
-						}
-			return $val;
-		} // Ende Ausgabefunktion
+	function ausgabe($where)
+	{
+		GLOBAL $tabelle;
+		// Ausgabe Konfig für die SQL Abfrage
+		$max = 300;  					// Max Anzahl anzeigen
+		$order = "order by klasse, rank "; 	// Nach Rang sortieren
+		$groupby = "  ";  				// Optional z.B. "group by rank", dann wird jeder Rank nur mit einem Namen belegt
+		$sql = "SELECT * FROM ".$tabelle." ".$where." ".$groupby." ".$order." LIMIT ".$max; 
+			#echo $sql;		
+			$val = '';
+			$resultX = mysql_query($sql);
+				  while ($row = mysql_fetch_object($resultX))
+					{
+					
+					if($row->klasse != $oldklasse)
+						$val .= '<tr><th colspan=2>'.id2class($row->klasse).'</th></tr>';
+					
+					$val .= '
+							<tr>								
+								<td valign=top>
+									<div class="char">
+									<div class="level">'.$row->level.'</div><div class="name"><b><a href ="http://eu.battle.net/wow/de/character/blackmoore/'.convutf8($row->name).'/advanced">'.convutf8($row->name).'</a>&nbsp;&nbsp;&nbsp;&nbsp;</b></div><div class="points"><img src="http://eu.battle.net/wow/static/images/icons/achievements.gif" alt=""> </img>'.$row->points.'</div>					
+									<div class="specimg"><img src="http://media.blizzard.com/wow/icons/36/'.$row->specimg.'.jpg"><img></div><div class="spec">'.$row->spec.'</div><div class="rank">'.id2rank($row->rank).'</div>
+									</div>
+									<div class="img-polaroid"><img src="http://eu.battle.net/static-render/eu/'.$row->tbnail.'"></img></div>
+								</td>
+							</tr>
+					';
+					
+					$oldklasse = $row->klasse;
+					
+					}
+		return $val;
+	} // Ende Ausgabefunktion
+	
 // Ausgabe
 echo '
 <!DOCTYPE html>
 <html lang="de">
-  <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="css/style.css" type="text/css"/>	
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css"/>	
 </head>
 ';
 
